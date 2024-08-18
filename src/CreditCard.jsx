92a41c6c6cf9de97c3b944b42a9e61ccba0d92cc
Bruno Francisco <64901068+brunofranciscu@users.noreply.github.com>
2024-08-18 17:06:45 -0300

    Update CreditCard.jsx

--- src/CreditCard.jsx
+++ src/CreditCard.jsx
@@ -19,9 +19,9 @@ export default function CreditCard({ conta }) {
    return (
       <div className='dark:bg-[#1f1f1f] rounded-xl w-full mx-auto py-2 px-7 flex items-start flex-col gap-1'>
          <h2 className='dark:text-[#F0F1F5] text-black text-md font-semibold'>Cartão de crédito</h2>
-         <small>Fatura atual</small>
-         <h3>{cc}</h3>
-         <small className="flex">Limite disponível de&nbsp;&nbsp;{limite}</small>
+         <small className='dark:bg-[#1f1f1f]'>Fatura atual</small>
+         <h3 className='dark:bg-[#1f1f1f]'>{cc}</h3>
+         <small className="flex dark:bg-[#1f1f1f]">Limite disponível de&nbsp;&nbsp;{limite}</small>
       </div>
    )
 }
